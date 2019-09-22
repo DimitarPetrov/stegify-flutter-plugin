53d494c73485dd086458a211ab2fd5d9533fa17c
Dimitar Petrov <buldozer9993@gmail.com>
2019-09-22 16:36:23 +0300

    API documentation

--- lib/flutter_stegify.dart
+++ lib/flutter_stegify.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/services.dart';
 
+/// Stegify is the top-level object that can be used to interact
+/// with the stegify native API (written in Go).
 class Stegify {
   static const MethodChannel _channel = const MethodChannel('flutter_stegify');
 
-  // encode performs steganography encoding of data file in carrier file and saves the steganography encoded product in new file.
+  /// encode performs steganography encoding of data file in carrier file and saves the steganography encoded product in new file.
   static void encode(String carrier, String data, String result) async {
     await _channel.invokeMethod("encode", [carrier, data, result]);
   }
 
-  // decode performs steganography decoding of data previously encoded by the Encode function.
-  // The data is decoded from file carrier and it is saved in separate new file
+  /// decode performs steganography decoding of data previously encoded by the Encode function.
+  /// The data is decoded from file carrier and it is saved in separate new file
   static void decode(String carrier, String result) async {
     await _channel.invokeMethod("decode", [carrier, result]);
   }
